a971001165dea749c4a23077539e1decd9405da9
Jheirom <jheirom.amp@gmail.com>
2024-11-05 14:42:55 +0400

    Create Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,35 @@
+fruits = ('apple', 'banana','Berry')
+print(fruits)
+
+print(fruits[1])
+print(fruits[-1])
+
+colors = ('red', 'green', 'blue')
+color1, color2, color3 = colors
+print(color1)
+print(color2)
+print(color3)
+
+
+numbers1 = (1,2,3)
+numbers2 = (4,5,6)
+
+combined = numbers1 + numbers2
+print(combined)
+
+
+alphabet = ('a','b','c','e','f','g',)
+print(alphabet[:3])
+print(alphabet[:-3])
+print(alphabet[:2])
+
+numbers =(1,2,2,3,4,4,5)
+print(numbers.count(4))
+print(numbers.count(2))
+
+nested = ( 1, 2, (3,4,), 5,)
+print(nested[2][1])
+
+colorss2 = ('red', 'green', 'blue')
+print('yellow' in colorss2)
+print('blue' in colorss2)
